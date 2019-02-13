ed3481cea54637f286e7e1f48effb35e4285b0d4
Daniel Reverol <daniel.reverol@gmail.com>
2019-02-13 14:40:32 -0400

    WIP: added firebase imports Ch40

---
+++ Store/Servicios/Firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDp9px0TZqm1SXHXI0ct6kkCPAh1hkgE8g',
+  authDomain: 'ezgram2073.firebaseapp.com',
+  databaseURL: 'https://ezgram2073.firebaseio.com',
+  projectId: 'ezgram2073',
+  storageBucket: 'ezgram2073.appspot.com',
+  messagingSenderId: '247054623387',
+};
+firebase.initializeApp(config);
