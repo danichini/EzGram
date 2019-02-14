eda547394b5401a3d72a85256d30c5d648388e58
Daniel Reverol <daniel.reverol@gmail.com>
2019-02-14 10:03:13 -0400

    WIP: implementacion de middleware Ch42

--- Store/Store.js
+++ Store/Store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as form } from 'redux-form';
 
 const reducerPrueba = (state = [0], action) => {
@@ -10,11 +10,21 @@ const reducerPrueba = (state = [0], action) => {
   }
 };
 
+const miMiddleWare = store => next => (action) => {
+  console.log('se ejecuta el middleware');
+  next(action);
+};
+
+const ultimoMiddleware = store => next => (action) => {
+  console.log('Ultimo middleware');
+  next(action);
+};
+
 const reducers = combineReducers({
   reducerPrueba,
   form,
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(miMiddleWare, ultimoMiddleware));
 
 export default store;
