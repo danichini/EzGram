8ccb83d6d56e52a69fa257bcd3ab586cdad7b56c
Daniel Reverol <daniel.reverol@gmail.com>
2019-02-19 15:52:32 -0400

    WIP: ImagePicker Ch53

--- Components/SeleccionarImagen.js
+++ Components/SeleccionarImagen.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import { Button, Image, View } from 'react-native';
+import {
+  Image, View, TouchableOpacity,
+} from 'react-native';
 import { ImagePicker } from 'expo';
+
+import picture from '../assets/profile.png';
 
 export default class SeleccionarImagen extends React.Component {
   state = {
     image: null,
   };
 
-  render() {
-    const { image } = this.state;
-
-    return (
-      <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          title="Pick an image from camera roll"
-          onPress={this._pickImage}
-        />
-        {image
-          && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      </View>
-    );
-  }
-
-  _pickImage = async () => {
+  pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       aspect: [4, 3],
@@ -34,4 +23,31 @@ export default class SeleccionarImagen extends React.Component {
       this.setState({ image: result.uri });
     }
   };
+
+  render() {
+    const { image } = this.state;
+
+    return (
+      <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
+        <TouchableOpacity
+          onPress={this.pickImage}
+        >
+          {
+          image ? (
+            <Image
+              source={{ uri: image }}
+              style={{ width: 200, height: 200, borderRadius: 100 }}
+            />
+          )
+            : (
+              <Image
+                style={{ width: 200, height: 200, borderRadius: 100 }}
+                source={picture}
+              />
+            )
+        }
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
